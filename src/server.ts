47f110988ec56e48e533fa971eebe66548aedce1
vHoffmann <vinicius.hoffram@hotmail.com>
2025-05-29 21:43:53 -0300

    create envs with zod validations

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions').select('*')
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
