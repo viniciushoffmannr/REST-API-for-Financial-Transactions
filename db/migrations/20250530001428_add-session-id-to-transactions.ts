873e3e21251a3c51cd035de939f04b79b4e802f0
vHoffmann <vinicius.hoffram@hotmail.com>
2025-05-29 21:18:49 -0300

    create table and editing with migrations

---
+++ db/migrations/20250530001428_add-session-id-to-transactions.ts
@@ -0,0 +1,14 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    // o index serve para gerar um indexador, para facilitar a busca, deixando-a mais rapida
+    table.uuid('session_id').after('id').index()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumn('session_id')
+  })
+}
